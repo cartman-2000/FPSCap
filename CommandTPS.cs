26e9b8f2cfb0256d7be3eb04d82faee00300fa17
cartman-2000 <cartman-2000@cartman-2000.net>
2015-07-28 15:28:23 -0700

    Added the TPS command to the plugin, to see what TPS the server is getting.

---
+++ CommandTPS.cs
@@ -0,0 +1,66 @@
+﻿//Rocket Mod TPS command used as a template for this command.
+using System;
+using System.Collections.Generic;
+using Rocket.API;
+using Rocket.Unturned.Chat;
+
+
+namespace FPSCap
+{
+    public class CommandTPS : IRocketCommand
+    {
+        public bool AllowFromConsole
+        {
+            get { return true; }
+        }
+
+        public string Name
+        {
+            get { return "tps"; }
+        }
+
+        public string Help
+        {
+            get { return "Shows the server's current TPS."; }
+        }
+
+        public string Syntax
+        {
+            get { return ""; }
+        }
+
+        public List<string> Aliases
+        {
+            get { return new List<string>(); }
+        }
+
+        public List<string> Permissions
+        {
+            get { return new List<string>() { "FPSCap.tps" }; }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            int timeLeft = (int)((DateTime.UtcNow - FPSCap.Started).TotalSeconds);
+            string runningTimeFormat = "";
+
+            // Format days, hours minutes and seconds since the server was started.
+            if (timeLeft >= (60 * 60 * 24))
+            {
+                runningTimeFormat = ((int)(timeLeft / (60 * 60 * 24))).ToString() + "d ";
+            }
+            if (timeLeft >= (60 * 60))
+            {
+                runningTimeFormat += ((int)((timeLeft / (60 * 60)) % 24)).ToString() + "h ";
+            }
+            if (timeLeft >= 60)
+            {
+                runningTimeFormat += ((int)((timeLeft / 60) % 60)).ToString() + "m ";
+            }
+            runningTimeFormat += ((int)(timeLeft % 60)).ToString() + "s";
+
+            UnturnedChat.Say(caller, FPSCap.Instance.Translations.Instance.Translate("tps_tps", FPSCap.TPS));
+            UnturnedChat.Say(caller, FPSCap.Instance.Translations.Instance.Translate("tps_running_since", FPSCap.Started.ToString(), runningTimeFormat));
+        }
+    }
+}
--- FPSCap.cs
+++ FPSCap.cs
@@ -4,18 +4,39 @@ using Rocket.API.Collections;
 using Rocket.Core.Logging;
 using Rocket.Core.Plugins;
 
-
 namespace FPSCap
 {
     public class FPSCap : RocketPlugin<FPSCapConfig>
     {
         public static FPSCap Instance;
+
+        public static float TPS;
+        private float updateInterval = 0.005f;
+        private float accum;
+        private int frames;
+        private float timeleft;
+        public static DateTime Started = DateTime.UtcNow;
+
         protected override void Load()
         {
             Instance = this;
             // Sets the default tps limit for the server to what has been set in the config.
             Application.targetFrameRate = Instance.Configuration.Instance.defaultTPS;
             Logger.Log(Translations.Instance.Translate("tps_set", Instance.Configuration.Instance.defaultTPS));
+        }
+
+        public void Update()
+        {
+            timeleft -= Time.deltaTime;
+            accum += Time.timeScale / Time.deltaTime;
+            frames++;
+            if (timeleft <= 0.0)
+            {
+                TPS = accum / frames;
+                timeleft = updateInterval;
+                accum = 0f;
+                frames = 0;
+            }
         }
 
         public override TranslationList DefaultTranslations
@@ -26,7 +47,9 @@ namespace FPSCap
                 {
                     { "invalid_arg", "Invalid Arguments." },
                     { "tps_set", "Server TPS has been set to: {0} TPS." },
-                    { "ltps_command_help", "<TPS> - Limits the server tps/fps to the value set in the command. 0 disables the TPS cap." }
+                    { "ltps_command_help", "<TPS> - Limits the server tps/fps to the value set in the command. 0 disables the TPS cap." },
+                    { "tps_tps", "TPS: {0}" },
+                    { "tps_running_since", "Running since: {0} UTC, running for: {1}" },
                 };
             }
         }
